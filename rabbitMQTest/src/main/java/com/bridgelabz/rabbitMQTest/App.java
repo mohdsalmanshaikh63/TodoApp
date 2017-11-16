da39c3b3455bd0f46ea4d81ae0f4349c27eded5d
salman <mohdsalmanshaikh63@gmail.com>
2017-11-16 19:21:53 +0530

    Implementing rabbitMQ Hello World

---
+++ rabbitMQTest/src/main/java/com/bridgelabz/rabbitMQTest/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.rabbitMQTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
