70225a51faac16b570200bafdafdae28febdded8
salman <mohdsalmanshaikh63@gmail.com>
2017-11-17 19:31:28 +0530

    Testing Java implementation of RabbitMq

---
+++ rabitMQ/src/com/bridgelabz/rabbitMQTest/workQueues/NewTask.java
@@ -0,0 +1,56 @@
+package com.bridgelabz.rabbitMQTest.workQueues;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+public class NewTask {
+
+	private static final String TASK_QUEUE_NAME = "task_queue";
+
+	public static void main(String[] argv) throws Exception {
+
+		String[][] messages = { { "First", "Message." }, { "Second", "Message.." }, { "Second", "Message..." },
+				{ "Third", "Message..." }, { "Fourth", "Message...." }, { "Fifth", "Message....." },
+				{ "sixth", "Message......" }, { "Seventh", "Message........" }, { "Eigth", "Message........" },
+				{ "Ninth", "Message.........." } };
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+		for (String[] strings : messages) {
+
+			String message = getMessage(strings);
+
+			channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN,
+					message.getBytes("UTF-8"));
+			System.out.println(" [x] Sent '" + message + "'");
+
+		}
+		channel.close();
+		connection.close();
+
+	}
+
+	private static String getMessage(String[] strings) {
+		if (strings.length < 1)
+			return "Hello World!";
+		return joinStrings(strings, " ");
+	}
+
+	private static String joinStrings(String[] strings, String delimiter) {
+		int length = strings.length;
+		if (length == 0)
+			return "";
+		StringBuilder words = new StringBuilder(strings[0]);
+		for (int i = 1; i < length; i++) {
+			words.append(delimiter).append(strings[i]);
+		}
+		return words.toString();
+	}
+}
